266c92fbe60cd8d645e875775b855643a1f1c7af
rachitdutt <rachit.rds.3636@gmail.com>
2020-06-11 21:47:30 +0530

    to do list app

---
+++ Events/effects.js
@@ -0,0 +1,13 @@
+// $('button').click(function(){
+// 	$('div').fadeOut(1000,function(){
+// 		$(this).remove();
+// 	});
+// });
+
+// $('button').click(function(){
+// 	$('div').fadeToggle()
+// 	});
+
+$('button').click(function(){
+	$('div').slidedown()
+	});
